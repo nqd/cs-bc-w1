1206226864defdeed890a1ae415aa88390c2ea6f
nqd <dinh.nguyen@veriksystems.com>
2018-03-18 15:31:08 +0700

    client work

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
+const headers = new Headers({
+  "Content-Type": "application/json",
+});
+
 function voteForCandidate() {
   candidateName = $('#candidate').val()
   contractInstance.voteForCandidate(candidateName, { from: web3.eth.accounts[0] }, function () {
@@ -11,10 +15,6 @@ function createGame() {
   const minBet = parseFloat($('#gameMinBet').val())
   const votes = parseInt($('#gameVote').val())
 
-  const headers = new Headers({
-    "Content-Type": "application/json",
-  });
-
   fetch('/game', {
     method: 'post',
     headers: headers,
@@ -23,20 +23,59 @@ function createGame() {
       triggerCall: votes
     }),
   })
-  .then(res => res.json())
-  .then(res => {
-    console.log('createGame response', res)
-  }).catch(function (e) {
-    // Error
-    console.log(e)
-  });
+    .then(res => res.json())
+    .then(res => {
+      console.log('createGame response', res)
+    }).catch(function (e) {
+      // Error
+      console.log(e)
+    });
 }
 
-$(document).ready(function () {
-  candidateNames = Object.keys(candidates)
-  for (var i = 0; i < candidateNames.length; i++) {
-    let name = candidateNames[i]
-    let val = contractInstance.totalVotesFor.call(name).toString()
-    $('#' + candidates[name]).html(val)
-  }
-})
+function bet0() { bet(0) }
+function bet1() { bet(1) }
+function bet2() { bet(2) }
+function bet3() { bet(3) }
+function bet4() { bet(4) }
+
+function bet(i) {
+  const player = parseInt($(`#player${i}`).val())
+  const bet = parseFloat($(`#bet${i}`).val())
+  const number = parseInt($(`#number${i}`).val())
+
+  fetch('/bet', {
+    method: 'post',
+    headers: headers,
+    body: JSON.stringify({
+      account: player,
+      number: number,
+      value: bet
+    }),
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log('bet response', res)
+      updatePlayerBalances()
+    }).catch(function (e) {
+      // Error
+      console.log(e)
+    });
+}
+
+function updatePlayerBalances() {
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  fetch('/balances', {
+    method: 'get',
+    headers: headers
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      for (let i = 0; i < 10; i++) {
+        document.getElementById(`balance${i}`).innerHTML = Math.round(res[i] * 100) / 100
+      }
+    })
+}
